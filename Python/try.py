fa6a181bfdd20a41cd1dfc69f183df2290cfd467
Leo Li <leoli_082000@hotmail.com>
2025-06-18 23:01:59 -0400

    add terribly strange question's solution attempt

---
+++ Python/try.py
@@ -0,0 +1,39 @@
+# import time
+
+# class Solution:
+#     def __init__(self, hour, values):
+#         self.initial_hour = time.time / 60 / 60
+#         self.index = 0
+#         self.all_responses = []
+
+#     def time_association(self, str: stringInput):
+#         # all in past hour as single csv string
+#         # return string []
+        
+#         # then hour prior to that
+#         # [zero to 1, ]
+#         # Every hour window gets its own
+#         current_hour = time.time / 60 / 60
+#         index = self.initial_hour - current_hour
+#         # earliest strings placed at the end
+#         parsed_string = ','.join(stringInput)
+  
+#         return all_responses[index] if len(all_responses) > index else return 
+
+
+# p1 = Solution("John", 36)
+
+# example_func("hello")
+
+# # return ["hello"]
+# # indices associated with time
+
+
+# # say at 1025, called with hello1
+
+# example_func("hello1")
+
+# # return ["hello1, hello"]
+# example_func("hello2")
+
+# # return ["hello2, hello1", "hello"]
